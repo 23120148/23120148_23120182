04c41370a46f154408f485bd6b180d10b30d6e3a
hotel-management <trantrucngoc2005@gmail.com>
2026-02-13 10:36:59 +0700

    Remove test.js from main branch

--- test.js
+++
@@ -1,7 +0,0 @@
-// Intentional vulnerability for SAST demo
-
-const DB_PASSWORD = "root123";
-const AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
-
-const input = location.hash;
-document.write(input);
